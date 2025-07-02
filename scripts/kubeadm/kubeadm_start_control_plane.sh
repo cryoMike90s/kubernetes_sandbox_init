3097e422723bfb17b530cde90e9685ebabb2dab0
miskers <miskers@protonmail.com>
2025-07-02 11:34:01 +0000

    sanbox_init
    update calico to 3.30.2

--- scripts/kubeadm/kubeadm_start_control_plane.sh
+++ scripts/kubeadm/kubeadm_start_control_plane.sh
@@ -5,4 +5,4 @@ set -e
 kubeadm init --config ./configs/kubeconfig-control-plane.yaml
 
 # Project Calico
-#kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.30.2/manifests/calico.yaml
